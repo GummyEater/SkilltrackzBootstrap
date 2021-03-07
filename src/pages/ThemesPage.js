b217c9503de61ed9d79de264301049b8f5d2819e
Brandon Casamichana <brandoncasa8@gmail.com>
2021-03-07 04:55:36 -0500

    Updated model

--- src/pages/ThemesPage.js
+++ src/pages/ThemesPage.js
@@ -1,16 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Button } from "react-bootstrap";
 import "./ThemesPage.scss";
 
 class Themes extends React.Component {
   render() {
-    return <div>themes</div>;
+    return (
+      <div>
+        <span>
+          <h1>Current Theme:</h1>
+          <h3>{this.props.current}</h3>
+        </span>
+        <Button onClick={() => this.props.changeCurrentTheme("lux", "dark")}>
+          Change to Lux
+        </Button>
+      </div>
+    );
   }
 }
 
-const mapState = (state) => ({});
+const mapState = (state) => ({
+  current: state.theme.current,
+  currentMode: state.theme.currentMode,
+});
 
-const mapDispatch = (dispatch) => ({});
+const mapDispatch = (dispatch) => ({
+  changeCurrentTheme: (newTheme, newMode) =>
+    dispatch.theme.changeCurrentTheme([newTheme, newMode]),
+});
 
 const ThemesPage = connect(mapState, mapDispatch)(Themes);
 
